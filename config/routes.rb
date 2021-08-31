7f82b99320d6acffdccc545e3482a82299d4b6ed
Luis Munoz <osbox68@gmail.com>
2021-08-30 23:42:07 -0400

    Add Article Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+  resources :articles, except: :index
+  # ⚠️ don't forget to replace `articles_path` by `root_path` on all the "Back" `link_to`
 end
